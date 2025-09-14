a2b64e006adfdce90d9e307332309a91f6e82a5c
Joshua Levin <127493106+webgefaehrten@users.noreply.github.com>
2025-09-15 00:28:17 +0200

    Fix

--- src/Models/Lock.php
+++ src/Models/Lock.php
@@ -13,6 +13,14 @@ class Lock extends Model
     protected $fillable = ['locked_by', 'locked_at'];
 
     /**
+     * Casts
+     * - locked_at als Carbon-Instanz, damit Zeitdifferenzen korrekt berechnet werden können
+     */
+    protected $casts = [
+        'locked_at' => 'datetime',
+    ];
+
+    /**
      * DE: Polymorphe Beziehung zur gesperrten Model-Instanz.
      * EN: Polymorphic relation to the locked model instance.
      */
